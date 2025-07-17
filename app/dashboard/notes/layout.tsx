9212446f757aa65ba465d30f4163aa273c33057e
Idnk <efeidukpaye@gmail.com>
2025-07-17 10:22:10 +0100

    feat: enhance notes layout with empty state and improved styling

--- app/dashboard/notes/layout.tsx
+++ app/dashboard/notes/layout.tsx
@@ -8,10 +8,14 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Id } from "@/convex/_generated/dataModel";
 import { useParams } from "next/navigation";
+import Image from "next/image";
+import NotFound from "../../../public/images/not-found-home.svg";
 
 export default function NotesLayout({ children }: { children: ReactNode }) {
   const notes = useQuery(api.notes.fetchNote);
   const { noteId } = useParams<{ noteId: Id<"note"> }>(); // Assuming you have a way to get the current note ID, e.g., from URL params
+
+  const hasNotes = notes && notes.length > 0;
 
   return (
     <main className="w-full space-y-8">
@@ -19,25 +23,39 @@ export default function NotesLayout({ children }: { children: ReactNode }) {
         <h1 className="text-4xl font-bold">Notes</h1>
         <CreateNoteButton />
       </div>
+      {hasNotes && (
+        <div className="flex gap-12">
+          <ul className="space-y-2 w-[300px]">
+            {notes?.map((note) => (
+              <li
+                key={note._id}
+                className={cn("text-base hover:text-cyan-100", {
+                  "text-cyan-300": note._id === noteId,
+                })}
+              >
+                <Link href={`/dashboard/notes/${note._id}`}>
+                  {note.text.substring(0, 24) + "..."}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-      <div className="flex gap-12">
-        <ul className="space-y-2 w-[300px]">
-          {notes?.map((note) => (
-            <li
-              key={note._id}
-              className={cn("text-base hover:text-cyan-100", {
-                "text-cyan-300": note._id === noteId,
-              })}
-            >
-              <Link href={`/dashboard/notes/${note._id}`}>
-                {note.text.substring(0, 24) + "..."}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <div className="bg-slate-800 rounded p-4 w-full">{children}</div>
-      </div>
+          <div className="w-full">{children}</div>
+        </div>
+      )}
+      {!hasNotes && <EmptyState />}
     </main>
   );
 }
+
+const EmptyState = () => (
+  <div className="flex flex-col justify-center items-center w-full">
+    <div className="max-w-xl flex justify-center relative max-">
+      <Image alt="child savings avatar" src={NotFound} className="" />{" "}
+    </div>
+    <div className=" my-4">
+      <h2>You don&apos;t have any notes</h2>
+    </div>
+    {/* <CreateDocumentBtn /> */}
+  </div>
+);
--- app/dashboard/notes/page.tsx
+++ app/dashboard/notes/page.tsx
@@ -1,7 +1,7 @@
 export default function NotesPage() {
   return (
-    <main className="flex flex-col items-center justify-between ">
-      <p className="">Please select a note</p>
+    <main className="flex flex-col items-center justify-between bg-slate-500 rounded p-4 ">
+      <p className=" font-bold text-xl">Please select a note</p>
     </main>
   );
 }
